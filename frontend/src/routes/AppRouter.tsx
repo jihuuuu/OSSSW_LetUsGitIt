f42e50ca7d3aafd62155a2de84972ba6d1c78149
sunggyeong <surreal0122@naver.com>
2025-05-22 13:11:58 +0900

    스크랩 조회 페이지 구현(#24)

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import NotePage from "../pages/NotePage"; // 노트 페이지 import
-
+import ScrapbookPage from "@/pages/ScrapbookPage";  //스크랩 페이지
 export default function AppRouter() {
   return (
     <Router>
@@ -9,6 +9,7 @@ export default function AppRouter() {
         <Route path="/" element={<div>홈입니다</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/users/notes" element={<NotePage />} />
+        <Route path="/users/scraps" element={<ScrapbookPage />} />
       </Routes>
     </Router>
   );
--- frontend/src/types/note.ts
+++ frontend/src/types/note.ts
@@ -1,7 +1,7 @@
 // src/types/note.ts
-export interface Note {
-  id: number;
+export type Note = {
+  id: string;
   title: string;
   content: string;
-  createdAt: string; // ISO 문자열 타입 (ex: 2024-05-21T12:00:00Z)
-}
+  createdAt: string;
+};
